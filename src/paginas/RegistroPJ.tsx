6f8d1008fe43bb5820ecbb42fb5689f4fe15ef65
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2023-11-17 22:21:53 -0300

    Configurando o RegistroPJ

--- src/paginas/RegistroPJ.tsx
+++ src/paginas/RegistroPJ.tsx
@@ -7,7 +7,23 @@ function RegistroPJ(){
         <div>
             <Cabecalho2/>
             <div className='container'>
-                <p>CADASTRAR-SE</p>
+                <div className='container-pj'>
+                <label>Conta Corporativa</label>
+                    <div className='Conta-corporativa1'>
+                        <input type="text" placeholder='Nome da Empresa'></input>
+                        <input type="text" placeholder='CNPJ'></input>
+                        <input type="text" placeholder='Inscrição Municipal'></input>
+                        <input type="text" placeholder='Telefone'></input>
+                    </div>
+                    <div className='Conta-corporativa2'>
+                        
+                        <input type="text" placeholder='CEP'></input>
+                        <input type="text" placeholder='Endereço'></input>
+                        <input type="text" placeholder='Bairro'></input>
+                        <input type="text" placeholder='Cidade'></input>
+                        <input type="text" placeholder='Estado'></input>
+                    </div>
+                </div>
             </div>
             <Rodape/>
         </div>
