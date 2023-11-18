f9d30afbda23b7b04e07d2e393b76f71b7098893
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2023-11-17 22:32:37 -0300

    css update

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -5,12 +5,18 @@ function MenuLateral() {
         <div className='container-menu'>
 
             <div className='imagem-usuario'>
-                <img src="./DogIcon.png" alt="imagem-usuario" />
+                <img src="./imagens/DogIcon.png" alt="imagem-usuario" />
+            </div>
+
+            <div className='nome-usuario'>
+                <label> Andre Comegno </label>
             </div>
 
             <div className='menu-button'>
-                <button type="button">Dashboard</button>
-                <button type="button">Dashboard</button>
+                <button type="button">Dashboard</button>                
+                <button type="button">Acompanhar</button>
+                <button type="button">Histórico</button>
+                <button type="button">Configurações</button>
             </div>
         </div>
     )
