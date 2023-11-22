d59358caae26804b6959d3f3457ae702e2032c16
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2023-11-22 19:47:46 -0300

    Criando a Página NotFound e subinda ela na rota

---
+++ src/paginas/NotFound.tsx
@@ -0,0 +1,16 @@
+import Rodape from '../components/rodape';
+import Cabecalho2 from '../components/cabecalho2';
+
+function NotFound(){
+    return(
+        <div>
+            <Cabecalho2/>
+            <div>
+                <p>Página not Found, Erro 404</p>
+            </div>
+            <Rodape/>
+        </div>
+    )
+}
+
+export default NotFound
