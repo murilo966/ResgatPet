b266ff7811bc03aa56886d214fe7b6590041ced6
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2023-11-24 21:57:08 -0300

    Upando para o André

--- src/paginas/Home/QuemSomos.tsx
+++ src/paginas/Home/QuemSomos.tsx
@@ -8,20 +8,21 @@ function QuemSomos(){
             <div className='container'>
                 
                 <div className='imagem'>
-                <img src="/imagens/DogIcon.png" className='imgleft'/>
-                <div className='titulo1'><label>Quem Somos</label> </div>
+                    <img src="/imagens/DogIcon.png" className='imgleft'/>
                 </div>
                 
                   
 
                 <div className='text2'> 
-                    <div ><p  >Somos um grupo de alunos do curso de desenvolvimento full stack do Senac Bauru 2023-2024, que, motivados pelo amor aos animais, decidimos desenvolver um site para auxiliar o resgate de animais em situação de rua e abandono.  </p></div>
-                <p  >Acreditamos que todos os animais merecem ter uma vida digna, com amor e cuidados ,por isso, criamos um espaço virtual que conecta ONGs e sociedade para o bem em comum. </p>
-                <p  >Nosso site é uma ferramenta que facilita o processo de resgate de animais, ele permite que ONGs registrem os animais em situação de rua ou abandono, e que pessoas interessadas em adotar ou doar podem encontrar os animais que precisam de um lar.</p><br/>
-                </div>
-
-                <div className='text2'><p ><h1 >Nossa missão</h1></p>
-                <p > Nossa missão é contribuir para a redução do número de animais em situação de rua e abandono, acreditamos que, com a união de esforços, podemos construir um mundo mais justo e amoroso para os animais.</p><br/>
+                    <div >
+                        <p  >Somos um grupo de alunos do curso de desenvolvimento full stack do Senac Bauru 2023-2024, que, motivados pelo amor aos animais, decidimos desenvolver um site para auxiliar o resgate de animais em situação de rua e abandono.  </p></div>
+                        <p  >Acreditamos que todos os animais merecem ter uma vida digna, com amor e cuidados ,por isso, criamos um espaço virtual que conecta ONGs e sociedade para o bem em comum. </p>
+                        <p  >Nosso site é uma ferramenta que facilita o processo de resgate de animais, ele permite que ONGs registrem os animais em situação de rua ou abandono, e que pessoas interessadas em adotar ou doar podem encontrar os animais que precisam de um lar.</p><br/>
+                    </div>
+                
+                <div className='text2'>
+                    <p ><h1 >Nossa missão</h1></p>
+                    <p > Nossa missão é contribuir para a redução do número de animais em situação de rua e abandono, acreditamos que, com a união de esforços, podemos construir um mundo mais justo e amoroso para os animais.</p><br/>
                 </div>
 
                 <div className='text2'>
@@ -41,32 +42,6 @@ function QuemSomos(){
                 </div>
 
                 <div className='text2'><p><h1>Juntos, podemos fazer a diferença na vida dos animais!</h1></p></div>
-                <p>QUEM SOMOS</p>
-                <div className='imagem'>
-                <img src="/imagens/DogIcon.png" className='imgleft'/>
-                <div className='div-img1'> <p className='titulo1'>Quem Somos </p></div>
-                                    
-      
-            
-                </div>
-
-                <div> <p className='text2'>Somos um grupo de alunos do curso de desenvolvimento full stack do Senac Bauru 2023-2024, que, motivados pelo amor aos animais, decidimos desenvolver um site para auxiliar o resgate de animais em situação de rua e abandono.  </p></div>
-                <p className='text2'>Acreditamos que todos os animais merecem ter uma vida digna, com amor e cuidados. Por isso, criamos um espaço virtual que conecta ONGs e sociedade para o bem em comum. </p>
-                <p className='text2'>Nosso site é uma ferramenta que facilita o processo de resgate de animais. Ele permite que ONGs registrem os animais em situação de rua ou abandono, e que pessoas interessadas em adotar ou doar podem encontrar os animais que precisam de um lar.</p>
-                <p className='text2'><h1>Nossa missão</h1></p>
-                <p className='text2'> Nossa missão é contribuir para a redução do número de animais em situação de rua e abandono. Acreditamos que, com a união de esforços, podemos construir um mundo mais justo e amoroso para os animais.</p>
-                <p className='text2'><h1>Nossos valores</h1></p>
-                <p className='text2'>Amor: amamos os animais e queremos o melhor para eles.</p>
-                <p className='text2'>Compaixão: somos solidários aos animais que sofrem.</p>
-                <p className='text2'>Responsabilidade: assumimos o compromisso de ajudar os animais.</p>
-                <p className='text2'>Cooperação: trabalhamos juntos para alcançar nossos objetivos.</p>
-                <p className='text2'><h1>Como nos ajudar</h1></p>
-                <p className='text2'>Você pode nos ajudar de várias maneiras:</p>
-                <p className='text2'>Adotando um animal do nosso site.</p>
-                <p className='text2'>Doando para uma ONG que esteja cadastrada no nosso site.</p>
-                <p className='text2'>Divulgando nosso site para que mais pessoas possam conhecer o nosso trabalho.</p>
-                <p className='text2'>Obrigado por sua ajuda!</p>
-                <p className='text2'><h2>Juntos, podemos fazer a diferença na vida dos animais.</h2></p>
             </div>
             <Rodape/>
         </div>
