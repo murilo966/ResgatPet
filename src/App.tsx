c4a61f37a2f3a2848e658ea379f00473602e4b2d
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2023-12-16 16:45:07 -0300

    correção nome das pastas
    correção nome das pastas

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Cadastrar from './paginas/autenticacao/Cadastrar';
-import ComoAjudar from './paginas/chome/ComoAjudar';
-import Home from './paginas/chome/Home';
+import ComoAjudar from './paginas/home/ComoAjudar';
+import Home from './paginas/home/Home';
 import Login from './paginas/autenticacao/Login';
-import QuemSomos from './paginas/chome/QuemSomos';
+import QuemSomos from './paginas/home/QuemSomos';
 import EsqueceuSenha from './paginas/autenticacao/EsqueceuSenha';
-import Dasboard from './paginas/cdashboard/Dashboard';
-import Acompanhar from './paginas/cdashboard/Acompanhar';
-import Historico from './paginas/cdashboard/Historico';
-import Configuracao from './paginas/cdashboard/Configuracao';
-import ResgatarPet from './paginas/cdashboard/ResgatarPet';
-import NotFound from './paginas/cerros/NotFound';
-import Formulario from './paginas/chome/Formulario';
+import Dasboard from './paginas/dashboard/Dashboard';
+import Acompanhar from './paginas/dashboard/Acompanhar';
+import Historico from './paginas/dashboard/Historico';
+import Configuracao from './paginas/dashboard/Configuracao';
+import ResgatarPet from './paginas/dashboard/ResgatarPet';
+import NotFound from './paginas/erros/NotFound';
+import Formulario from './paginas/home/Formulario';
 
 
 function App() {
