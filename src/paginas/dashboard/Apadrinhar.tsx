14ee341e24cf5f9df071f53205f532d8ec9d4bf1
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-02-13 12:59:27 -0300

    cards versao mobile, add novas paginas, icones
    - adicionado a versão mobile da pagina dashboard com os cards menores
    - adicionado novas paginas no dashboard
    - adicionado a pagina acompanhar, doação, apadrinhar, ongs e parceiros
    - adicionado novos ícones no menu dashboad pets, instituição, doação
    - adicionado menu cascata no dashboard
    - foram renomeado alguns ícones para melhor entendimento
    - correcao de bugs

---
+++ src/paginas/dashboard/Apadrinhar.tsx
@@ -0,0 +1,17 @@
+import MenuLateral from "../../components/menu-lateral"
+
+function Apadrinhar() {
+    return (
+        <div className='container'>
+            <div className='dashboard'>
+                <MenuLateral />
+
+                <div className='container-painel'>
+                    <h1>Apadrinhar </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Apadrinhar
