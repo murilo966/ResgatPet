7806bd5125271511967b02bd1b50390d814868bb
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-03-11 22:21:35 -0300

    Criando pagina de doação
    falta adicionar o qr code correto

--- src/paginas/dashboard/Doacao.tsx
+++ src/paginas/dashboard/Doacao.tsx
@@ -1,4 +1,6 @@
 import MenuLateral from "../../components/menu-lateral"
+import FotoQrcode1 from "../../assents/imagens/card-apadrinhar/QrcodeTeste.png"
+import FotoQrcode2 from "../../assents/imagens/card-apadrinhar/QrcodeTeste.png"
 
 function Doacao() {
     return (
@@ -6,12 +8,30 @@ function Doacao() {
             <div className='dashboard'>
                 <MenuLateral />
 
-                <div className='container-painel'>
-                    <h1>Doacao </h1>
+                <div className="container-painel">
+                    <div className="doacoes">
+
+                        <h1>Contribua para o Nosso Site</h1>
+                        <p>
+                            Você se comove profundamente com os assuntos que abordamos em nosso site, não é mesmo? Cada palavra escrita, 
+                            cada imagem compartilhada, tem o poder de tocar corações e inspirar mudanças significativas. Sua contribuição
+                             não apenas apoia a continuidade desse importante trabalho, mas também é um gesto de solidariedade e empatia.
+                        </p>
+                        <div className="qr-codes">
+                            <div className="qr-code">
+                                <img src={FotoQrcode1} alt="QR Code 1" />
+                                <p>Contribuição via Pix</p>
+                            </div>
+                            <div className="qr-code">
+                                <img src={FotoQrcode2} alt="QR Code 2" />
+                                <p>Contribuição via Pix</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+)
 }
 
 export default Doacao
