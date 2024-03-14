7deb4b6534847f4140b67282f8b3f4b89e217fe5
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-03-13 22:23:48 -0300

    localStorage add ao login
    localStorage add ao login

--- src/components/navegador-botoes/index.tsx
+++ src/components/navegador-botoes/index.tsx
@@ -1,10 +1,8 @@
 import logo from '../../assents/imagens/logo/ic_resgatpet.png'
-import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { UsuarioLogadoContext } from '../../context/authContext'
 
 function Menu() {
-    const auth = useContext(UsuarioLogadoContext)
+    const auth = localStorage.getItem('ContextEmail')
     const navigate = useNavigate()
 
     const handleHome = () => {
@@ -42,17 +40,12 @@ function Menu() {
 
                     <button onClick={handleQuemSomos}>Quem Somos</button>
 
-                    {/* {auth.user && <button onClick={hadleDashboard}>Dashboard</button>}
+                    {!auth && <button onClick={hadleDashboard}>Dashboard</button>}
 
-                    {auth.user && <button onClick={handleLogout}>Sair</button>}
+                    {!auth && <button onClick={handleLogout}>Sair</button>}
 
-                    {!auth.user &&<button onClick={handleLogin}>Faça login</button>} */}
+                    {auth &&<button onClick={handleLogin}>Faça login</button>}
 
-                    <button onClick={hadleDashboard}>Dashboard</button>                    
-
-                    <button onClick={handleLogin}>Faça login</button>
-
-                    <button onClick={handleLogout}>Sair</button>
                 </div>
             </div>
         </div>
