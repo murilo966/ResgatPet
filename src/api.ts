e287d1c9a5c876e9ec32216e54b6999e11250730
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-04-10 22:32:01 -0300

    add server

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,12 @@
 export const api = {
 
     // LOGIN
-    Logar: async (email: string, senha: string) => {
-        // const response = await fetch("https://resgat-pet-api.vercel.app/usuarios/login" ,{
-        const response = await fetch('http://localhost:3005/usuarios/login', {
+    Logar: async (EMAIL: string, SENHA: string) => {
+        const response = await fetch('https://resgatpetapi.onrender.com/usuarios/login', {
             method: 'POST',
             body: JSON.stringify({
-                email: email,
-                senha: senha
+                EMAIL: EMAIL,
+                SENHA: SENHA
             }),
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
@@ -18,17 +17,16 @@ export const api = {
         return (json)
     },
     // CRIAR NOVA CONTA 
-    CriarConta: async (nome: string, cpf_cnpj: string, telefone: string, email: string, senha: string, level: string) => {
-        // const response = await fetch('https://resgat-pet-api.vercel.app/usuarios', {
-        const response = await fetch('http://localhost:3005/usuarios', {
+    CriarConta: async (NOMECOMPLETO: string, CPF_CNPJ: string, TELEFONE: string, EMAIL: string, SENHA: string, LEVEL: string) => {
+        const response = await fetch('https://resgatpetapi.onrender.com/usuarios', {
             method: 'POST',
             body: JSON.stringify({
-                nome: nome,
-                cpf_cnpj: cpf_cnpj,
-                telefone: telefone,
-                email: email,
-                senha: senha,
-                level: level
+                NOMECOMPLETO: NOMECOMPLETO,
+                CPF_CNPJ: CPF_CNPJ,
+                TELEFONE: TELEFONE,
+                EMAIL: EMAIL,
+                SENHA: SENHA,
+                LEVEL: LEVEL
             }),
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
@@ -40,27 +38,25 @@ export const api = {
     },
     // CARREGAR TODOS OS FORMULARIOS
     CarregarTodosFormularios: async () => {
-        // const response = await fetch("https://resgat-pet-api.vercel.app/formulario")
-        const response = await fetch("http://localhost:3005/formulario")
+        const response = await fetch("https://resgatpetapi.onrender.com/formulario")
         let json = await response.json()
         return json
     },
     // NOVO FORMULARIO
-    CriarFormulario: async (petFoto: string, petEndereco: string, petCidade: string, petRaca: string,
-        petSexo: string, petCor: string, petSaude: string, petAcessorios: string, usuario?: string) => {
-        // const response = await fetch('https://resgat-pet-api.vercel.app/formulario', {
-        const response = await fetch('http://localhost:3005/formulario', {
+    CriarFormulario: async (IMAGEM: string, ENDERECO: string, CIDADE: string, RACA: string,
+        SEXO: string, COR: string, SAUDE: string, ACESSORIO: string, USUARIO?: string) => {
+        const response = await fetch('https://resgatpetapi.onrender.com/formulario', {
             method: 'POST',
             body: JSON.stringify({
-                fotoPet: petFoto,
-                endereco: petEndereco,
-                cidade: petCidade,
-                raca: petRaca,
-                sexo: petSexo,
-                cor: petCor,
-                saude: petSaude,
-                acessorio: petAcessorios,
-                usuario: usuario,
+                IMAGEM: IMAGEM,
+                ENDERECO: ENDERECO,
+                CIDADE: CIDADE,
+                RACA: RACA,
+                SEXO: SEXO,
+                COR: COR,
+                SAUDE: SAUDE,
+                ACESSORIO: ACESSORIO,
+                USUARIO: USUARIO,
             }),
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
@@ -71,7 +67,7 @@ export const api = {
         return (json)
     },
     UpdateImage: async (formData: FormData) => {
-        let response = await fetch('http://localhost:3005/arquivos', {
+        let response = await fetch('https://resgatpetapi.onrender.com/arquivos', {
             method: 'POST',
             body: formData
         });
